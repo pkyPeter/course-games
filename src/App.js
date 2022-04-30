3f1d364299c3087d566b738d21374921292f9aa6
Peter Pan <peter.pan@shoplineapp.com>
2022-04-30 19:32:56 +0800

    feat: add react-router

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Guess, Home, Matches } from "pages";
+import Sidebar from "components/Sidebar";
 
 function App() {
   return (
-    <div className="App">
-
-
+    <div className="App h-screen w-screen">
+      <BrowserRouter>
+        <Sidebar />
+        <div className="px-6 py-4">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="guess" element={<Guess />} />
+            <Route path="matches" element={<Matches />} />
+          </Routes>
+        </div>
+      </BrowserRouter>
     </div>
   );
 }
---
+++ src/pages/index.js
@@ -0,0 +1,3 @@
+export { default as Guess } from "./Guess";
+export { default as Home } from "./Home";
+export { default as Matches } from "./Matches";
