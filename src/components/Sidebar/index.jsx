b8e0793380b804cffc048d76190e8efd7252fcde
Peter Pan <peter.pan@shoplineapp.com>
2022-04-30 19:33:15 +0800

    feat: add sidebar and font-family

---
+++ src/components/Sidebar/index.jsx
@@ -0,0 +1,38 @@
+import React from "react";
+import { NavLink } from "react-router-dom";
+
+const getNavLinkClassNames = ({ isActive }) => {
+  let classNames = "py-2 px-3 ";
+  if (isActive) classNames += "border-b-2	border-main-green";
+  return classNames;
+};
+
+const Sidebar = () => {
+  const tabs = [
+    {
+      to: "/",
+      label: "首頁",
+    },
+    {
+      to: "guess",
+      label: "猜猜看",
+    },
+    {
+      to: "matches",
+      label: "找出一對",
+    },
+  ];
+  return (
+    <div className="flex gap-[0.3rem] py-2 pl-6">
+      {tabs.map((tab) => {
+        return (
+          <NavLink className={getNavLinkClassNames} to={tab.to}>
+            {tab.label}
+          </NavLink>
+        );
+      })}
+    </div>
+  );
+};
+
+export default Sidebar;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,10 @@
 module.exports = {
-  content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
-    "./index.html"
-  ],
+  content: ["./src/**/*.{js,jsx,ts,tsx}", "./index.html"],
   theme: {
+    colors: {
+      "main-green": "#3da4aa",
+    },
     extend: {},
   },
   plugins: [],
-}
+};
