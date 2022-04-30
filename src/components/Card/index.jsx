0c8b6646a5a395a5ca0c2aad812368bbf86bdaf9
Peter Pan <peter.pan@shoplineapp.com>
2022-04-30 22:32:22 +0800

    feat(Card): add url src for github pages

--- src/components/Card/index.jsx
+++ src/components/Card/index.jsx
@@ -1,6 +1,8 @@
 import classNames from "classnames";
 import CardImg from "assets/music.png";
 
+const imageBase = process.env.NODE_ENV === "development" ? "" : "/course-games";
+
 const Card = ({ flipped, imgSrc, name, onClick, onClose, onRemove }) => {
   const clickHandler = (e) => {
     onClick(e, name);
@@ -20,10 +22,10 @@ const Card = ({ flipped, imgSrc, name, onClick, onClose, onRemove }) => {
         )}
       >
         <div className="flip-card-front rounded-md absolute w-full h-full backface-hidden">
-          <img src={CardImg} alt="Card" />
+          <img src={imageBase + CardImg} alt="Card" />
         </div>
         <div className="flip-card-back rounded-md absolute w-full h-full backface-hidden rotateY-180">
-          <img src={imgSrc} alt="樂器" />
+          <img src={imageBase + imgSrc} alt="樂器" />
           <div className="flex justify-center gap-4 absolute bottom-3 w-full">
             <button
               className="rounded border text-main-green px-1.5 py-1 border-main-green bg-white hover:bg-main-green-opacity-6 hover:text-white hover:border-white"
